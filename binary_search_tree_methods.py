35ed9e60e717cf75b30fb1e1deb49318c53fa6d8
vagrant <vagrant@vagrant.vm>
2017-08-05 09:35:04 +0000

    working on methods for binary search tree

---
+++ binary_search_tree_methods.py
@@ -0,0 +1,123 @@
+class Node(object):#helper class
+
+     def __init__(self, data, leftchild=None, rightchild=None): #constructor method 
+          self.data = data
+          self.leftchild = leftchild
+          self.rightchild = rightchild
+
+
+     def insert(self, new_data):
+
+          if self.data: 
+               if new_data < self.data:
+                    if self.leftchild is None: 
+                         self.leftchild = Node(new_data)
+                    else:
+                         self.leftchild.insert(new_data)
+
+               elif new_data > self.data:
+                    if self.rightchild is None: 
+                         self.rightchild = Node(new_data)
+                    else: 
+                         self.rightchild.insert(new_data)
+          else: 
+               self.data = new_data
+
+
+     def find(self, value):
+
+          if(self.data==value):
+               return True
+
+          elif self.data > value:
+               if self.leftchild:
+                    return self.leftchild.find(value)
+
+               else:
+                    return False
+
+          else: 
+               if self.rightchild:
+                    return self.rightchild.find(value)
+               else:
+                    return False
+
+
+
+class Binary_Search_Tree(object):#main user interface
+
+     def __init__(self, root=None):
+          self.root = root #define root 
+
+
+     def add_value(self, value):
+          #add values to bst, one by one without using helper class, or recursion
+          if self.root == None:
+               self.root = Node(value)
+
+          else:
+               current = self.root
+
+               while True:
+                    if value < current.data:
+                         if current.leftchild:
+                              current = current.leftchild
+                         else:
+                              current.leftchild = Node(value)
+                              break
+
+                    elif value > current.data:
+                         if current.rightchild:
+                              current = current.rightchild
+                         else:
+                              current.rightchild = Node(value)
+                              break
+
+                    else: 
+                         break
+
+     def insert(self, data):
+     #this insert method calls the insert method from the node class
+          if self.root:
+               return self.root.insert(data)
+
+          else:
+               self.root = Node(data)
+               return True
+
+
+
+     def find(self, value):
+     #this find method calls the find method from the node class
+
+          if self.root:
+               return self.root.find(value)
+
+          else:
+               return False 
+
+
+
+#these are the tree traversals outside of objects
+def preorder(root):
+
+     if root is not None:
+          print root.data,
+          preorder(root=root.leftchild)
+          preorder(root=root.rightchild)
+
+def postorder(root):
+
+     if root is not None:
+          postorder(root.leftchild)
+          postorder(root.rightchild)
+          print root.data,
+
+def inorder(root):
+
+     if root is not None:
+          inorder(root.leftchild)
+          print root.data
+          inorder(root.rightchild)
+
+
